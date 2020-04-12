f2dd3990addc2c20eaf1a43ce1b9f3fe3c39cfde
Tejas Rana <tejas.rana@tikaj.com>
2020-04-13 05:00:36 +0530

    [update] nav and footer

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('port', (process.env.PORT || config.port));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, "www/partials"));
-//app.use(express.static(path.join(__dirname, "ui/pages")));
+app.use(express.static(path.join(__dirname, "ui/assets")));
 
 hbs.registerPartials(path.join(__dirname, "www/partials"));
 
@@ -26,7 +26,6 @@ class API {
             else
                 pagePath = "index";
             fs.readFile(path.join(__dirname, "ui/pages/" + pagePath + ".html"), 'utf-8', (err, html) => {
-                fs.writeFile(path.join(__dirname, 'www/partials/page.hbs'), html, 'utf-8', (err1) => {
                     res.render('header', {
                         title: this.nav.sub.filter((data) => {
                             return data.name.toLowerCase() === pagePath;
@@ -34,9 +33,8 @@ class API {
                             return data.name;
                         }),
                         nav: config.nav.sub,
-                        pageData:html
+                        html:html
                     });
-                });
             });
         }).listen(app.get('port'), () => {
             console.log(style.success("Running on port " + app.get('port')));
